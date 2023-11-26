811dca05001fda40675f96719b5e1b488c671555
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-11-26 22:02:24 +0100

    Fixed bug, where object was modified before being used in part 2

--- 2022/rust/day01/src/main.rs
+++ 2022/rust/day01/src/main.rs
@@ -5,11 +5,12 @@ fn main() {
         .split("\n\n")
         .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
         .collect::<Vec<u32>>();
+    let elves_part2 = elves.clone();
 
     let part1 = elves.iter().max().unwrap();
     println!("{part1}");
 
-    let mut part2 = elves.clone();
+    let mut part2 = elves_part2;
     part2.sort();
     let part2 = part2.into_iter().rev().take(3).sum::<u32>();
     println!("{part2}");
