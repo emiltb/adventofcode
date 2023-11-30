68b6b968dc1e05860c8b87e1a12241fe61ceb157
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-11-30 14:51:40 +0100

    Updated download script with header and year

--- get_input.py
+++ get_input.py
@@ -6,7 +6,7 @@ import argparse
 AOC_COOKIE = {"session": os.getenv("AOCCOOKIE")}
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--year", type=int, default=2022)
+parser.add_argument("--year", type=int, default=2023)
 parser.add_argument("--day", type=int, default=1)
 parser.add_argument("--force", action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
@@ -18,9 +18,16 @@ if os.path.exists(input_path) and not args.force:
     print(f"Input for {args.year} day {args.day} already downloaded")
     exit(1)
 
+headers = {"User-Agent": "github.com/emiltb/adventofcode by emilbp@gmail.com"}
+
 data = requests.get(
-    f"https://adventofcode.com/{args.year}/day/{args.day}/input", cookies=AOC_COOKIE
+    f"https://adventofcode.com/{args.year}/day/{args.day}/input",
+    cookies=AOC_COOKIE,
+    headers=headers,
 )
+
+print(data.request.headers)
+
 with open(input_path, "w") as f:
     f.writelines(data.text)
 
