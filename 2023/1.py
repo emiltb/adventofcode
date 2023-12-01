9ac536c7adb4608c30a5a4705b9ea1b285e9f6c7
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-01 11:59:02 +0100

    Cleaned up unnecessary int conversions

--- 2023/1.py
+++ 2023/1.py
@@ -9,17 +9,17 @@ print(sum(numbers))
 
 # Part 2
 all_digits = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9",
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
 }
-all_digits.update({v: v for d, v in all_digits.items()})
+all_digits.update({str(v): v for d, v in all_digits.items()})
 
 
 def get_first_last_digits(s):
@@ -28,9 +28,9 @@ def get_first_last_digits(s):
         for m in re.finditer(d, s):
             indexes.append((m.start(), v))
     vals = sorted(indexes, key=lambda tup: tup[0])
-    return (int(vals[0][1]), int(vals[-1][1]))
+    return (vals[0][1], vals[-1][1])
 
 
 digits = [get_first_last_digits(s) for s in data]
-numbers = [int(n[0]) * 10 + int(n[-1]) for n in digits]
+numbers = [n[0] * 10 + n[-1] for n in digits]
 print(sum(numbers))
