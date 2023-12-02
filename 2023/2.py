c1d64c51f142913df50d16ed70c53be325776760
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-02 20:44:36 +0100

    Run everything in one loop

--- 2023/2.py
+++ 2023/2.py
@@ -4,7 +4,7 @@ data = [l.strip() for l in open("data/2.in")]
 games = [l.split(":")[1].split(";") for l in data]
 
 
-# Part 1
+# Part 1 and 2
 def get_n_colors(s):
     n_colors = tuple()
     for c in ["green", "blue", "red"]:
@@ -15,7 +15,7 @@ def get_n_colors(s):
     return n_colors
 
 
-total = 0
+part1 = part2 = 0
 for i, g in enumerate(games):
     max_red = max_green = max_blue = 0
     for draw in g:
@@ -25,20 +25,9 @@ for i, g in enumerate(games):
         max_red = max(red, max_red)
 
     if max_red <= 12 and max_green <= 13 and max_blue <= 14:
-        total += i + 1
+        part1 += i + 1
 
-print(total)
+    part2 += max_green * max_blue * max_red
 
-# Part 2
-total = 0
-for i, g in enumerate(games):
-    max_red = max_green = max_blue = 0
-    for draw in g:
-        green, blue, red = get_n_colors(draw)
-        max_green = max(green, max_green)
-        max_blue = max(blue, max_blue)
-        max_red = max(red, max_red)
-
-    total += max_green * max_blue * max_red
-
-print(total)
+print(part1)
+print(part2)
