1e06bf087c41da4a37b17df05c363e322062e69a
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-02 20:36:50 +0100

    Solutions for 2023 day 2

---
+++ 2023/2.py
@@ -0,0 +1,44 @@
+import re
+
+data = [l.strip() for l in open("data/2.in")]
+games = [l.split(":")[1].split(";") for l in data]
+
+
+# Part 1
+def get_n_colors(s):
+    green = re.search("(\d+) green", s)
+    green = int(green.group(1)) if green else 0
+    blue = re.search("(\d+) blue", s)
+    blue = int(blue.group(1)) if blue else 0
+    red = re.search("(\d+) red", s)
+    red = int(red.group(1)) if red else 0
+    return (green, blue, red)
+
+
+total = 0
+for i, g in enumerate(games):
+    max_red = max_green = max_blue = 0
+    for draw in g:
+        green, blue, red = get_n_colors(draw)
+        max_green = green if green > max_green else max_green
+        max_blue = blue if blue > max_blue else max_blue
+        max_red = red if red > max_red else max_red
+
+    if max_red <= 12 and max_green <= 13 and max_blue <= 14:
+        total += i + 1
+
+print(total)
+
+# Part 2
+total = 0
+for i, g in enumerate(games):
+    max_red = max_green = max_blue = 0
+    for draw in g:
+        green, blue, red = get_n_colors(draw)
+        max_green = green if green > max_green else max_green
+        max_blue = blue if blue > max_blue else max_blue
+        max_red = red if red > max_red else max_red
+
+    total += max_green * max_blue * max_red
+
+print(total)
