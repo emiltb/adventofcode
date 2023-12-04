b793c7c51eb2381e5b67a5edc5df21acab015ac8
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-04 12:06:31 +0100

    Solution for 2023 day 1 part 2

--- 2023/4.py
+++ 2023/4.py
@@ -1,5 +1,20 @@
+from collections import deque
+
 # Part 1
 data = [[set([int(s) for s in n.split(" ") if s != ""]) for n in l.strip().split(":")[1].split("|")] for l in open("data/4.in")]  # fmt: skip
 
-winning_numbers = [1 * 2 ** (len(s1 & s2) - 1) for s1, s2 in data if len(s1 & s2) > 0]
+winning_sets = {i: len(s1 & s2) for i, (s1, s2) in enumerate(data, 1)}
+winning_numbers = [1 * 2 ** (s - 1) for s in winning_sets.values() if s > 0]
 print(sum(winning_numbers))
+
+# Part 2
+q = deque(winning_sets.keys())
+
+total_cards = 0
+while q:
+    card = q.popleft()
+    total_cards += 1
+    for i in range(1, winning_sets[card] + 1):
+        q.appendleft(card + i)
+
+print(total_cards)
