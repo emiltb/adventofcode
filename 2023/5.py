080119f8d70a77e2da37a5091704252542bd729d
JB4792 Emil Tveden Bjerglund <jb4792@jyskebank.dk>
2023-12-06 12:19:48 +0100

    Revised day 5 part 2 for a 7615x speed improvement

--- 2023/5.py
+++ 2023/5.py
@@ -1,6 +1,9 @@
-data = [l.strip() for l in open("data/5.test.in")]
+from time import time
+
+data = [l.strip() for l in open("data/5.in")]
 
 # Part 1
+start_time = time()
 seeds = [[int(n) for n in s.split()] for s in data[0].split(":")[1:]][0]
 
 maps = dict()
@@ -26,8 +29,10 @@ for s in seeds:
         s = convert(k, s)
     min_loc = min(s, min_loc)
 print(min_loc)
+print(f"Part 1 time: {time() - start_time:.5f} s")
 
 # Part 2
+start_time = time()
 seed_pairs = list(zip(*(iter(seeds),) * 2))
 
 
@@ -49,82 +54,57 @@ while search:
             print(init_l)
             search = False
     l = init_l + 1
+part2_time = time() - start_time
+print(f"Part 2 time: {part2_time:.5f} s")
+
 
 # Part 2 - revised
-
-
-def contains(a, b):
-    # Returns true if b is contained in a
-    return (a[0] <= b[0]) and (a[1] >= b[1])
+start_time = time()
 
 
 def convert_range(map_type, rng):
-    if isinstance(rng[0], list):
-        l = []
-        for e in rng:
-            l.append(convert_range(map_type, e))
-        return l
+    converted_ranges = []
 
-    r_start, r_len = rng
-    r_end = r_start + r_len
     for d_start, s_start, m_len in maps[map_type]:
-        d_end = d_start + m_len
         s_end = s_start + m_len
-        # print(f"comparing {s_start}-{s_end} -> {d_start}-{d_end}")
-        # print(f"with {r_start}-{r_end} ->")
 
-        if contains([s_start, s_end], [r_start, r_end]):
-            # print("seed range is contained")
-            return [
-                r_start - s_start + d_start,
-                r_end - s_end + d_end - (r_start - s_start + d_start),
-            ]
-        if r_start < s_start < r_end and r_end < s_end:
-            # print("seed range overlaps on the left")
-            r1 = [r_start, (s_start - 1) - r_start]
-            c1 = convert_range(map_type, r1)
-            r2 = [s_start, r_end - s_start]
-            c2 = convert_range(map_type, r2)
-            return [c1, c2]
-        if s_start < r_start and r_start < s_end < r_end:
-            # print("seed range overlaps on the right")
-            r1 = [r_start, r_start - s_start]
-            c1 = convert_range(map_type, r1)
-            r2 = [s_end, r_end - s_end]
-            c2 = convert_range(map_type, r2)
-            return [c1, c2]
-        if r_start < s_start and s_end < r_end:
-            # print("seed range exceeds in both ends")
-            r1 = [r_start, (s_start - 1) - r_start]
-            r2 = [s_start, s_end - s_start]
-            r3 = [s_end + 1, r_end - (s_end + 1)]
-            c1 = convert_range(map_type, r1)
-            c2 = convert_range(map_type, r2)
-            c3 = convert_range(map_type, r3)
-            return [c1, c2, c3]
+        new_ranges = []
+        while rng:
+            start, end = rng.pop()
 
-    # print("everything else should fall through unaltered")
-    # for d_start, s_start, r_len in maps[map_type]:
-    #     if s_start <= key <= (s_start + r_len):
-    #         return d_start - s_start + key
-    return rng
+            interval_before = (start, min(end, s_start))
+            interval_contained = (max(start, s_start), min(s_end, end))
+            interval_after = (max(s_end, start), end)
+
+            if interval_before[1] > interval_before[0]:
+                new_ranges.append(interval_before)
+            if interval_contained[1] > interval_contained[0]:
+                converted_ranges.append(
+                    (
+                        interval_contained[0] - s_start + d_start,
+                        interval_contained[1] - s_start + d_start,
+                    )
+                )
+            if interval_after[1] > interval_after[0]:
+                new_ranges.append(interval_after)
+
+        rng = new_ranges
+    return rng + converted_ranges
 
 
-res = convert_range("seed-to-soil", (10, 100))
-res
+loc_ranges = []
+for start, size in seed_pairs:
+    rng = [(start, start + size)]
 
-min_loc = 1e12
-for s in seed_pairs:
-    print(s)
-    sc = s
     for k in maps.keys():
-        sc = convert_range(k, sc)
-        if len(sc) > 2:
-            sc = list(zip(*(iter(sc),) * 2))
-        print(sc)
+        rng = convert_range(k, rng)
 
+    loc_ranges.append(rng)
 
-#     for k in maps.keys():
-#         s = convert(k, s)
-#     min_loc = min(s, min_loc)
-# print(min_loc)
+loc_ranges_flat = [y for x in loc_ranges for y in x]
+min_loc = min([m for m, _ in loc_ranges_flat])
+print(min_loc)
+
+part2_time_revised = time() - start_time
+print(f"Part 2 time (revised): {part2_time_revised:.5f} s")
+print(f"Improvement: {part2_time/part2_time_revised:.2f}x")
