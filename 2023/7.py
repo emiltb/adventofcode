a0f761306c88a0a1bb5c0752a616bf25f46a412a
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-07 11:16:28 +0000

    Slightly refactored, as code handled unnecessary edge-case

--- 2023/7.py
+++ 2023/7.py
@@ -4,12 +4,11 @@ from itertools import count
 
 data = {k: int(b) for k, b in [l.split() for l in open("data/7.in")]}
 
-# Part 1
 def s_compare(s, find_max=False):
     def compare(a, b):
         max_a, max_b = a, b
         if find_max:
-            max_a, max_b = [find_max_equivalent_card(x) for x in [a, b]]
+            max_a, max_b = [find_max_equivalent_card(s, x) for x in [a, b]]
         la, lb = [sorted(Counter(l).values(), reverse=True) for l in [max_a, max_b]]
         if la == lb:
             ia, ib = [[s.index(c) for c in x] for x in [a, b]]
@@ -20,10 +19,11 @@ def s_compare(s, find_max=False):
         return -1
     return compare
 
-def find_max_equivalent_card(d):
-    new_cards = [d.replace("J", s) for s in new_symbols[1:]]
-    return sorted(new_cards, key=cmp_to_key(s_compare(new_symbols)))[-1]
+def find_max_equivalent_card(s, d):
+    new_cards = [d.replace("J", s) for s in s[1:]]
+    return sorted(new_cards, key=cmp_to_key(s_compare(s)))[-1]
 
+# Part 1
 symbols = "AKQJT98765432"[::-1]
 sorted_cards = sorted(data.keys(), key=cmp_to_key(s_compare(symbols)))
 P1 = sum(data[card] * rank for card, rank in zip(sorted_cards, count(1)))
