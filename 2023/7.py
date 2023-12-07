8a753623de6507c599cd35006de8264dcb0f2f1b
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-07 08:19:35 +0000

    Refactored for readability

--- 2023/7.py
+++ 2023/7.py
@@ -4,7 +4,6 @@ from itertools import count
 
 data = {k: int(b) for k, b in [l.split() for l in open('data/7.in')]}
 symbols = "AKQJT98765432"[::-1]
-c = count(1)
 
 def compare(a,b):
     la, lb = [sorted(Counter(l).values(), reverse = True) for l in [a,b]]
@@ -12,15 +11,11 @@ def compare(a,b):
         ia, ib = [[symbols.index(c) for c in x] for x in [a,b]]
         if ia > ib:
             return 1
-        else:
-            return -1
     if la > lb:
         return 1
-    if la < lb:
-        return -1
+    return -1
     
-sorted_cards = sorted(list(data.keys()), key = cmp_to_key(compare))
+sorted_cards = sorted(data.keys(), key = cmp_to_key(compare))
 
-P1 = sum(data[card] * rank for card, rank in zip(sorted_cards, c))
+P1 = sum(data[card] * rank for card, rank in zip(sorted_cards, count(1)))
 print(P1)
-
