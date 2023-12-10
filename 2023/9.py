bd560ff4533dff7f0af269a6daf0d25704f90a89
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-10 09:19:30 +0100

    Recursive solution for 2023 day 9

--- 2023/9.py
+++ 2023/9.py
@@ -21,3 +21,13 @@ for l in data:
     P2.append(r2)
 print(sum(P1))
 print(sum(P2))
+
+# On the day I could not make the proper recursive
+# solution to this problem work, but here it is
+def g(l):
+    next_line = [s2-s1 for s1, s2 in zip(l, l[1:])]
+    return l[-1] + g(next_line) if next_line else 0
+
+for d in [1,-1]:
+    print(sum(g(l[::d]) for l in data))
+
