77b4a7db98194dc7ef8c9de3a4f218f04d6d246a
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-11 08:58:54 +0000

    Solution for 2023 day 11 part 2

--- 2023/11.py
+++ 2023/11.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 data = [list(l.strip()) for l in open('data/11.in')]
 
+# Part 1
 empty_rows = [i for i, l in enumerate(data) if all(s=='.' for s in l)]
 empty_cols = [i for i, l in enumerate([all([s[i]=='.' for s in data]) for i in range(len(data[0]))]) if l]
 
@@ -23,4 +24,22 @@ for (ax,ay),(bx,by) in combinations(galaxies, 2):
     P1 += d
 print(P1)
 
+# Part 2
+data = [list(l.strip()) for l in open('data/11.in')]
+galaxies = []
+for r, l in enumerate(data):
+    for c, s in enumerate(l):
+        if s == "#":
+            galaxies.append((r,c))
 
+new_galaxies = []
+for gr,gc in galaxies:
+    ngr = gr + sum([r < gr for r in empty_rows]) * (1000000 - 1)
+    ngc = gc + sum([c < gc for c in empty_cols]) * (1000000 - 1)
+    new_galaxies.append((ngr,ngc))
+
+P2 = 0
+for (ax,ay),(bx,by) in combinations(new_galaxies, 2):
+    d = abs(ax-bx) + abs(ay-by)
+    P2 += d
+print(P2)
