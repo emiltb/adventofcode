20cb73d4ddcb87e15cc29c9b3b48c74458f14588
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-12 20:54:31 +0100

    Solution for 2023 day 12 part 1

--- 2023/12.py
+++ 2023/12.py
@@ -1,40 +1,54 @@
 from ast import literal_eval
-import re
-from itertools import permutations
 
 data = [
     [a, literal_eval(b)] for a, b in [l.strip().split(" ") for l in open("data/12.in")]
 ]
 
 
-def get_arrangement(l):
-    damaged = re.findall("#+", l)
-    return tuple(d.count("#") for d in damaged)
+def f(string, damaged):
+    # print(f"string={string}, damaged={damaged}")
+    ans = 0
 
+    if len(damaged) == 0 and "#" in string:
+        return 0
 
-def f(l):
-    springs, damaged = l
-    n_springs = len(springs)
-    n_damaged = sum(damaged)
-    n_missing = springs.count("?")
-    n_known_working = springs.count(".")
-    n_known_damaged = springs.count("#")
-    n_working = n_springs - n_damaged
+    if len(damaged) == 0 and not "#" in string:
+        # print("Found a solution!")
+        return 1
 
-    ways = set(permutations(["."] * (n_working - n_known_working) + ["#"] * (n_damaged - n_known_damaged), n_missing))
-    c = 0
-    for w in ways:
-        new_s = springs
-        for s in w:
-            new_s = new_s.replace('?', s, 1)
-        if get_arrangement(new_s) == damaged:
-            c += 1
+    if len(string) == 0:
+        if damaged:
+            return 0
+        # print("Found a solution!")
+        return 1
 
-    return c
+    if string[0] == ".":
+        ans += f(string[1:], damaged)
+
+    if string[0] == "?":
+        for c in ["#", "."]:
+            new_string = string.replace("?", c, 1)
+            ans += f(new_string, damaged)
+
+    if string[0] == "#":
+        len_next_block = damaged[0]
+        if len_next_block > len(string):
+            return 0
+        # print(f"Len of next block: {len_next_block}; Len string: {len(string)}")
+
+        if not "." in string[:len_next_block] and (
+            len(string) == len_next_block or string[len_next_block] != "#"
+        ):
+            new_string = string[len_next_block + 1 :]
+            ans += f(new_string, damaged[1:])
+
+    return ans
 
 
 P1 = 0
-for i, l in enumerate(data):
-    print(i)
-    P1 += f(l)
+for s, d in data:
+    ans = f(s, d)
+    # print(s, d, ans)
+    P1 += ans
+
 print(P1)
