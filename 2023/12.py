4abee87130dd3ab2980961adcacea725f08291fa
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-12 21:24:59 +0100

    2023 day 12 refactored for readability

--- 2023/12.py
+++ 2023/12.py
@@ -12,20 +12,16 @@ def f(string, damaged, cache):
     if key in cache:
         return cache[key]
 
-    # print(f"string={string}, damaged={damaged}")
     ans = 0
 
-    if len(damaged) == 0 and "#" in string:
-        return 0
-
-    if len(damaged) == 0 and not "#" in string:
-        # print("Found a solution!")
+    if len(damaged) == 0:
+        if "#" in string:
+            return 0
         return 1
 
     if len(string) == 0:
         if damaged:
             return 0
-        # print("Found a solution!")
         return 1
 
     if string[0] == ".":
@@ -40,13 +36,11 @@ def f(string, damaged, cache):
         len_next_block = damaged[0]
         if len_next_block > len(string):
             return 0
-        # print(f"Len of next block: {len_next_block}; Len string: {len(string)}")
 
         if not "." in string[:len_next_block] and (
             len(string) == len_next_block or string[len_next_block] != "#"
         ):
-            new_string = string[len_next_block + 1 :]
-            ans += f(new_string, damaged[1:], cache)
+            ans += f(string[len_next_block + 1 :], damaged[1:], cache)
 
     cache[key] = ans
     return ans
@@ -57,7 +51,6 @@ P1 = 0
 for s, d in data:
     cache.clear()
     P1 += f(s, d, cache)
-
 print(P1)
 
 
