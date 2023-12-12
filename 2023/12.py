9e5ff4b3bf9dda02940f2a83b96a9b48c5661a2f
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-12 21:11:48 +0100

    Solution for 2023 day 12 part 2

--- 2023/12.py
+++ 2023/12.py
@@ -4,8 +4,14 @@ data = [
     [a, literal_eval(b)] for a, b in [l.strip().split(" ") for l in open("data/12.in")]
 ]
 
+cache = {}
 
-def f(string, damaged):
+
+def f(string, damaged, cache):
+    key = (string, damaged)
+    if key in cache:
+        return cache[key]
+
     # print(f"string={string}, damaged={damaged}")
     ans = 0
 
@@ -23,12 +29,12 @@ def f(string, damaged):
         return 1
 
     if string[0] == ".":
-        ans += f(string[1:], damaged)
+        ans += f(string[1:], damaged, cache)
 
     if string[0] == "?":
         for c in ["#", "."]:
             new_string = string.replace("?", c, 1)
-            ans += f(new_string, damaged)
+            ans += f(new_string, damaged, cache)
 
     if string[0] == "#":
         len_next_block = damaged[0]
@@ -40,15 +46,26 @@ def f(string, damaged):
             len(string) == len_next_block or string[len_next_block] != "#"
         ):
             new_string = string[len_next_block + 1 :]
-            ans += f(new_string, damaged[1:])
+            ans += f(new_string, damaged[1:], cache)
 
+    cache[key] = ans
     return ans
 
 
+# Part 1
 P1 = 0
 for s, d in data:
-    ans = f(s, d)
-    # print(s, d, ans)
-    P1 += ans
+    cache.clear()
+    P1 += f(s, d, cache)
 
 print(P1)
+
+
+# Part 2
+P2 = 0
+for s, d in data:
+    cache.clear()
+    s = "?".join([s] * 5)
+    d = tuple([x for y in [d] * 5 for x in y])
+    P2 += f(s, d, cache)
+print(P2)
