ead50726f786747a4e2f08d9be89bbe3a126bf52
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-13 19:37:03 +0100

    Solution for 2023 day 13 part 1

--- 2023/13.py
+++ 2023/13.py
@@ -1,22 +1,15 @@
-from itertools import count
-data = [l.split() for l in open('data/13.test.in').read().split('\n\n')]
+data = [l.split() for l in open("data/13.in").read().split("\n\n")]
 
-def f(d):
-    rmax = (0,0)
-    for i in range(len(d)-1):
-        n = 0
-        while True:
-            try:
-                if d[i - n] == d[i + 1 + n]:
-                    n += 1
-                else:
-                    break
-            except IndexError:
-                return n + 1
+
+def f(l):
+    for n in range(1, len(l)):
+        if all(l1 == l2 for l1, l2 in zip(l[:n][::-1], l[n:])):
+            return n
     return 0
+
 
 P1 = 0
 for l in data:
-    l_t = [''.join(i) for i in zip(*l)]
+    l_t = ["".join(i) for i in zip(*l)]
     P1 += 100 * f(l) + f(l_t)
 print(P1)
