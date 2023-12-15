3b1f9bdf3df85b74c267a2f50891b594fddb2c35
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-15 09:30:23 +0000

    Solution for 2023 day 15 part 2

--- 2023/15.py
+++ 2023/15.py
@@ -1,13 +1,35 @@
-data = "".join([l.strip() for l in open('data/15.in')])
+from collections import defaultdict
+data = "".join([l.strip() for l in open('data/15.in')]).split(',')
+
+# Part 1
+def f(e):
+    n = 0
+    for s in e:
+        n = ((n + ord(s)) * 17) % 256
+    return n
 
 P1 = 0
-n = 0
-for s in data:
-    if s == ",":
-        P1 += n
-        n = 0
-        continue
-    n = ((n + ord(s)) * 17) % 256
-else:
-    P1 += n
+for e in data:
+    P1 += f(e)
 print(P1)
+
+# Part 2
+boxes = defaultdict(dict)
+for e in data:
+    if "=" in e:
+        box, op = e.split("=")
+        lens = {box: int(op)}
+        if box in boxes[f(box)]:
+            boxes[f(box)][box] = int(op)
+        else: 
+            boxes[f(box)].update(lens)
+    if "-" in e:
+        box, _ = e.split('-')
+        if box in boxes[f(box)]:
+            boxes[f(box)].pop(box)
+
+P2 = 0
+for box, lenses in boxes.items():
+    for n, lens in enumerate(lenses.values(), 1):
+        P2 += (box + 1) * n * lens
+print(P2)
