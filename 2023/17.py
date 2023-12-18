343dce318f63aed263fe450b23060f5a842bc25c
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-18 21:52:13 +0100

    Solution for 2023 day 17 part 1 + partial solution for part 2

--- 2023/17.py
+++ 2023/17.py
@@ -1,58 +1,76 @@
-from collections import deque, defaultdict
-from pprint import pprint
+from collections import namedtuple
+from heapq import heappop, heappush
 
 data = [l.strip() for l in open("data/17.test.in")]
 
-data
+block = namedtuple("block", ["heatloss", "r", "c", "dr", "dc", "n"])
 
-S = (0, 0, 0, 0)
+# Part 1
+S = block(0, 0, 0, 0, 0, 0)
 T = (len(data) - 1, len(data[0]) - 1)
+q = [S]
+visited = set()
 
-q = deque([S])
-visited = defaultdict(lambda: 1e6)
-visited[S] = 0
-steps = defaultdict(list)
-trace = defaultdict(list)
-next_heat_loss = 0
 while q:
-    r, c, pdr, pdc = q.popleft()
-    # print(r, c, dr, pdc)
+    node = heappop(q)
 
-    if (r, c) == T:
-        print(next_heat_loss)
+    if (node.r, node.c) == T:
+        print(node.heatloss)
         break
 
-    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        next_r, next_c = r + dr, c + dc
+    if (node.r, node.c, node.dr, node.dc, node.n) in visited:
+        continue
+
+    visited.add((node.r, node.c, node.dr, node.dc, node.n))
+
+    for ndr, ndc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        next_r, next_c = node.r + ndr, node.c + ndc
         if next_r < 0 or next_r > len(data) - 1 or next_c < 0  or next_c > len(data[0]) - 1:  # fmt: skip
             continue
 
-        # print(next_r, next_c)
-
-        if all(s == (dr, dc) for s in steps[(r, c)][-3:]) and len(steps[(r, c)]) >= 3:
-            # print("Repeating too many steps")
-            # print((r, c), steps[(r, c)], (dr, dc))
+        if (ndr, ndc) == (-node.dr, -node.dc):
             continue
-        # print(next_r, next_c, data[next_r][next_c])
 
-        next_heat_loss = visited[(r, c, pdr, pdc)] + int(data[next_r][next_c])
-        # print(next_heat_loss)
-        if next_heat_loss <= visited[(next_r, next_c, dr, dc)]:
-            q.append((next_r, next_c, dr, dc))
-            visited[((next_r, next_c, dr, dc))] = next_heat_loss
-            steps[((next_r, next_c, dr, dc))] = steps[(r, c)] + [(dr, dc)]
-            trace[((next_r, next_c))] = trace[(r, c)] + [(next_r, next_c)]
+        n = 1 if (ndr, ndc) != (node.dr, node.dc) else node.n + 1
+        if n > 3:
+            continue
 
-print(visited[T])
+        heappush(q, block(node.heatloss + int(data[next_r][next_c]), next_r, next_c, ndr, ndc, n))  # fmt: skip
 
+# Part 2
+q = [S]
+visited = set()
 
-for r, l in enumerate(data):
-    for c, d in enumerate(l):
-        if (r, c) in trace[T]:
-            print(".", end="")
-        else:
-            print(d, end="")
-    print()
+while q:
+    node = heappop(q)
 
-pprint(data)
-trace[T]
+    if (node.r, node.c) == T:
+        print(node.heatloss)
+        break
+
+    if (node.r, node.c, node.dr, node.dc, node.n) in visited:
+        continue
+    print(node)
+
+    visited.add((node.r, node.c, node.dr, node.dc, node.n))
+
+    for ndr, ndc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        next_r, next_c = node.r + ndr, node.c + ndc
+        if next_r < 0 or next_r > len(data) - 1 or next_c < 0  or next_c > len(data[0]) - 1:  # fmt: skip
+            continue
+
+        if (ndr, ndc) == (-node.dr, -node.dc):
+            continue
+
+        n = 1 if (ndr, ndc) != (node.dr, node.dc) else node.n + 1
+
+        if n > 10:
+            continue
+
+        if n < 4 and (ndr, ndc) == (node.dr, node.dc) and (node.dr, node.dc) != (0, 0):
+            heappush(q, block(node.heatloss + int(data[next_r][next_c]), next_r, next_c, ndr, ndc, n))  # fmt: skip
+            continue
+
+        heappush(q, block(node.heatloss + int(data[next_r][next_c]), next_r, next_c, ndr, ndc, n))  # fmt: skip
+else:
+    print("Did not find T")
