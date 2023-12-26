7502b1f496a1c0179181fc2a28f0f377a9e94d51
Emil Tveden Bjerglund <emilbp@gmail.com>
2023-12-26 13:34:06 +0100

    Solution for 2023 day 23 part 2 (not sure if working)

--- 2023/23.py
+++ 2023/23.py
@@ -1,4 +1,5 @@
 from heapq import heappop, heappush
+from collections import defaultdict
 
 data = [l.strip() for l in open("data/23.in")]
 
@@ -40,24 +41,17 @@ print(P1)
 
 # Part 2
 
-q = [(0, *S, 0, 0, [])]
-visited = [q[0]]
+q = [(*S,)]
+visited = set(q)
+graph = defaultdict(lambda: defaultdict(int))
 
-P2 = 0
+
 while q:
-    steps, r, c, dr, dc, trace = heappop(q)
-    # print(steps, r, c, dr, dc, trace)
+    r, c = heappop(q)
 
-    if (r, c) == T:
-        P2 = max(-steps, P2)
-        print(P2)
-        continue
-
+    n_adjacent = 0
     for ndr, ndc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         next_r, next_c = r + ndr, c + ndc
-
-        if (dr, dc) == (-ndr, -ndc):
-            continue
 
         if next_r < 0 or next_r > len(data) - 1 or next_c < 0  or next_c > len(data[0]) - 1:  # fmt: skip
             continue
@@ -65,9 +59,45 @@ while q:
         if data[next_r][next_c] not in ".>v":
             continue
 
-        next_node = (steps - 1, next_r, next_c, ndr, ndc, trace + [(next_r, next_c)])
-        if next_node not in visited and (next_r, next_c) not in trace:
+        next_node = (next_r, next_c)
+        graph[(r, c)][next_node] = 1
+        graph[next_node][(r, c)] = 1
+        if next_node not in visited:
             heappush(q, next_node)
-            visited.append(next_node)
+            visited.add(next_node)
 
+
+intermediate_keys = {k for k, v in graph.items() if len(v) == 2}
+
+for node in intermediate_keys:
+    (n1, w1), (n2, w2) = graph[node].items()
+
+    new_weight = w1 + w2
+
+    graph[n1][n2] = new_weight
+    graph[n2][n1] = new_weight
+
+    graph[n1].pop(node)
+    graph[n2].pop(node)
+    graph.pop(node)
+
+
+q = [(0, *S, [])]
+visited = [(*S, [])]
+P2 = 0
+while q:
+    pl, r, c, trace = heappop(q)
+
+    if (r, c) == T:
+        new_max = max(P2, -pl)
+        if new_max > P2:
+            P2 = new_max
+            print(P2)
+
+    for edge_k, edge_v in graph[(r, c)].items():
+        next_node = (*edge_k, trace + [edge_k])
+
+        if next_node not in visited and edge_k not in trace:
+            visited += [next_node]
+            heappush(q, (pl - edge_v, *next_node))
 print(P2)
