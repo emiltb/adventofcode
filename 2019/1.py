5248177854ca510db4430df50d1437f7b85bf380
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-01-03 22:56:40 +0100

    More compact solution for 2019 day 1

--- 2019/1.py
+++ 2019/1.py
@@ -1,10 +1,5 @@
 data = [int(l.strip()) for l in open('data/1.in')]
 print(sum(d//3-2 for d in data))
 
-def f(n):
-    ans = n//3-2
-    if ans <= 0:
-        return 0
-    return ans + f(ans)
-
+f = lambda n: 0 if (ans := n//3-2) <= 0 else ans + f(ans)
 print(sum(f(d) for d in data))
