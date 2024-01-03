38b1939770ad3257693dc2c7db791eb46dba3be3
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-01-03 11:13:21 +0000

    Solution for 2023 day 24 part 2

--- 2023/24.py
+++ 2023/24.py
@@ -22,23 +22,21 @@ for (x1, y1, z1, vx1, vy1, vz1), (x2, y2, z2, vx2, vy2, vz2) in combinations(dat
 print(P1)
 
 # Part 2
-x, y, z, vx, vy, vz = variables = symbols('x,y,z,vx,vy,vz')
-
-equations = []
-time_symbols = []
-
 # p + v*t = p[i] + v[i]*t   =>   p + v*t - p[i] - v[i]*t = 0
 # We need to find 6 variables (x, y, z, vx, vy, vz) and 
 # the collission time for each hailstone.
 # This means that the system can be solved using 9 equations, describing
 # the collisions with 3 hailstones.
+
+x, y, z, vx, vy, vz = variables = symbols('x,y,z,vx,vy,vz')
+equations = []
 for i, (hx, hy, hz, hvx, hvy, hvz) in enumerate(data[:3]):
     t = Symbol(f"t{i}")
 
     equations.append(x + vx*t - hx - hvx*t)
     equations.append(y + vy*t - hy - hvy*t)
     equations.append(z + vz*t - hz - hvz*t)
-    time_symbols.append(t)
+    variables += (t,)
 
-P2 = solve_poly_system(equations, *variables, *time_symbols)[0]
+P2 = solve_poly_system(equations, *variables)[0]
 print(sum(P2[:3]))
