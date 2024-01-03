a0f49fc8df909c3a1e52185937516b3fe51e4463
Emil Tveden Bjerglund <emil@tbjerglund.dk>
2024-01-03 20:26:42 +0100

    Solution for 2023 day 21 part 2

--- 2023/21.py
+++ 2023/21.py
@@ -1,23 +1,40 @@
-from collections import deque
+from collections import deque, defaultdict
+import numpy as np
 
 data = [l.strip() for l in open("data/21.in")]
+steps = 26501365
+
+assert len(data) == len(data[0])
+size = len(data)
 
 S = [(0, r, c) for r, l in enumerate(data) for c, s in enumerate(l) if s == "S"]
 
 P1 = set()
+P2_params = []
 visited = set()
 q = deque(S)
+n_max = 0
 while q:
+    if q[0][0] % size == size // 2 and q[0][0] > n_max:
+        n_max = q[0][0]
+        P2_params.append(len(q))
+
     n, r, c = q.popleft()
+
     if n == 64:
         P1.add((r, c))
-        continue
+
     for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
         next_r, next_c = r + dr, c + dc
-        if data[next_r][next_c] != "#":
+        if data[next_r % size][next_c % size] != "#":
             next_node = (n + 1, next_r, next_c)
-            if next_node not in visited:
+            if next_node not in visited and n + 1 <= size * 3:
                 q.append(next_node)
                 visited.add(next_node)
 
 print(len(P1))
+
+# Part 2
+fit = np.array(np.polyfit([0,1,2], P2_params, 2), dtype=np.int64)
+grid_size = steps // size
+print(fit[0] * grid_size ** 2 + fit[1] * grid_size + fit[2])
