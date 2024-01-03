c910b5b5f80c5370c057812eba15a3cbd69169cb
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-01-03 09:46:51 +0000

    Solution for 2023 day 24 part 2

--- 2023/24.py
+++ 2023/24.py
@@ -1,5 +1,6 @@
 import re
 from itertools import combinations
+from sympy import Symbol, solve_poly_system
 
 data = [[int(n) for n in re.findall("-?\d+", l.strip())] for l in open("data/24.in")]
 
@@ -19,3 +20,21 @@ for (x1, y1, z1, vx1, vy1, vz1), (x2, y2, z2, vx2, vy2, vz2) in combinations(dat
     except ZeroDivisionError:
         pass
 print(P1)
+
+# Part 2
+x, y, z = Symbol('x'), Symbol('y'), Symbol('z')
+vx, vy, vz = Symbol('vx'), Symbol('vy'), Symbol('vz')
+
+equations = []
+time_symbols = []
+
+for i, (hx, hy, hz, hvx, hvy, hvz) in enumerate(data[:3]):
+    t = Symbol(f"t{i}")
+
+    equations.append(x + vx*t - hx - hvx*t)
+    equations.append(y + vy*t - hy - hvy*t)
+    equations.append(z + vz*t - hz - hvz*t)
+    time_symbols.append(t)
+
+P2 = solve_poly_system(equations, x, y, z, vx, vy, vz, *time_symbols)[0]
+print(sum(P2[:3]))
