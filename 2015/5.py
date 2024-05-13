3156b4d35e95b8a0c1cce98a3b6a346a06e7dee1
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-05-13 10:48:45 +0000

    Solution for 2015 day 5

--- 2015/5.py
+++ 2015/5.py
@@ -7,8 +7,19 @@ P1 = 0
 for s in data:
     c1 = len(re.findall('[aeiou]', s)) >= 3
     c2 = re.search(r"(.)\1", s)
-    c3 = [i for i in ['ab', 'cd', 'pq', 'xy'] if i in s]
+    c3 = re.search(r"ab|cd|pq|xy", s)
     if c1 and c2 and not c3:
         P1 += 1
 
 print(P1)
+
+# %%
+P2 = 0
+for s in data:
+    c1 = re.search(r"([a-z]{2})(.)*\1", s)
+    c2 = re.search(r"([a-z]{1})([a-z]){1}\1", s)
+    if c1 and c2:
+        P2 += 1
+
+print(P2)
+# %%
