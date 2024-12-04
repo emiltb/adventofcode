a572467727d70826b56098934863f809806fa42d
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-12-04 20:49:02 +0100

    2024 day 4 (python)

--- 2024/4.py
+++ 2024/4.py
@@ -66,6 +66,7 @@ def check_pos(p):
     r, c = p
     diag1 = data[r][c] + data[r+1][c+1] + data[r+2][c+2]
     diag2 = data[r+2][c] + data[r+1][c+1] + data[r][c+2]
-    return (diag1 in ('MAS','SAM')) and (diag2 in ('MAS','SAM'))
+    T = ('MAS','SAM')
+    return diag1 in T and diag2 in T
 
 print(sum(check_pos((r,c)) for r in range(rs-2) for c in range(cs-2)))
