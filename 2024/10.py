30f6abb6e214a886a6aaf10cb6c7c08adec23233
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-12-10 17:02:15 +0100

    2024 day 10 (python)

--- 2024/10.py
+++ 2024/10.py
@@ -1,36 +1,45 @@
 from collections import deque
 data = [l.strip() for l in open('data/10.in')]
 
-start = {(r,c): 0 for r, l in enumerate(data) for c, s in enumerate(l) if s == '0'}
-end = [(r,c) for r, l in enumerate(data) for c, s in enumerate(l) if s == '9']
-# print(data)
-# print(start)
-# print(end)
-
-q = deque(end)
-
+start = [(r,c) for r, l in enumerate(data) for c, s in enumerate(l) if s == '0']
 dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+rows, cols = len(data) - 1, len(data[0]) - 1
 
-while q:
+def num_trailheads(s):
+    res = 0
+    q = deque([s])
+    visited = set([s])
 
-    node = q.popleft()
-    lq = deque([node])
-    visited = set(node)
-    
-    while lq:
-        ln = lq.popleft()
+    while q:
+        r, c = q.popleft()
+
         for d in dirs:
-            next_r, next_c = ln[0] + d[0], ln[1] + ln[1]
-            if next_r < 0 or next_r > len(data) - 1 or next_c < 0 or next_c > len(data[0]) - 1:
-                break
-            if int(data[next_r][next_c]) + 1 != int(data[ln[0]][ln[1]]):
-                break
-            if (next_r, next_c) not in visited:
-                visited.add((next_r, next_c))
-                if data[next_r][next_c] == "0":
-                    start[(next_r,next_c)] += 1
+            next_r, next_c = r + d[0], c + d[1]
+            
+            if next_r < 0 or next_r > rows or next_c < 0 or next_c > cols or (next_r, next_c) in visited or int(data[next_r][next_c]) - int(data[r][c]) != 1:
+                continue
 
-print(start)
+            res += data[next_r][next_c] == "9"
+            q.append((next_r, next_c))
+            visited.add((next_r, next_c))
+    return res
 
+def num_distinct_trailheads(s):
+        res = 0
+        q = deque([(*s, [s])])
 
+        while q:
+            r, c, trace = q.popleft()
 
+            for d in dirs:
+                next_r, next_c = r + d[0], c + d[1]
+                
+                if next_r < 0 or next_r > rows or next_c < 0 or next_c > cols or (next_r, next_c) in trace or int(data[next_r][next_c]) - int(data[r][c]) != 1:
+                    continue
+
+                res += data[next_r][next_c] == "9"
+                q.append((next_r, next_c, trace + [(next_r, next_c)]))
+        return res
+
+print(sum(num_trailheads(s) for s in start))
+print(sum(num_distinct_trailheads(s) for s in start))
