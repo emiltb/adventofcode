e5541f6e594704b2df28334a6ac0170ec7b1bf56
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-12-19 07:05:56 +0100

    2024 day 19 (python)

--- 2024/19.py
+++ 2024/19.py
@@ -2,19 +2,19 @@ towels, designs = open('data/19.in').read().split('\n\n')
 towels = tuple(t.strip() for t in towels.split(','))
 designs = [d.strip() for d in designs.split()]
 
-def f(string, idx, cache):
-    if (string, idx) in cache: 
-        return cache[(string, idx)]
+def f(string, i, cache):
+    if (string, i) in cache: 
+        return cache[(string, i)]
     
     ans = 0
 
-    if len(string[idx:]) == 0: 
+    if len(string[i:]) == 0: 
         return 1
     
-    for r in [len(t) for t in towels if string[idx:].startswith(t)]: 
-        ans += f(string, idx + r, cache)
+    for r in [len(t) for t in towels if string[i:].startswith(t)]: 
+        ans += f(string, i + r, cache)
 
-    cache[(string, idx)] = ans
+    cache[(string, i)] = ans
     return ans
 
 print(sum(f(d, 0, {}) > 0 for d in designs))
