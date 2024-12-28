aa7eace78499aefae0e3fcdd33c4c3b6016ed14d
Emil Tveden Bjerglund <emilbp@gmail.com>
2024-12-28 21:10:43 +0100

    2024 day 20 partial solution for part 2

--- 2024/20.py
+++ 2024/20.py
@@ -1,5 +1,5 @@
-from collections import Counter, defaultdict
-data = [l.strip() for l in open('data/20.in')]
+from collections import Counter, defaultdict, deque
+data = [l.strip() for l in open('data/20.test.in')]
 grid = {(r,c) for r, l in enumerate(data) for c, s in enumerate(l) if s == '#'}
 start = [(r,c) for r, l in enumerate(data) for c, s in enumerate(l) if s == 'S'][0]
 
@@ -28,4 +28,31 @@ for r, c in inner_grid:
         n1, n2 = neighbors
         P1.append(abs(visited[n1] - visited[n2]) - 2)
 
-print(sum(v for k, v in Counter(P1).items() if k >= 100))
+print(sum(v for k, v in Counter(P1).items() if k >= 50))
+
+
+cheats = {}
+for pos in visited:
+    print(f"Check cheats starting at {pos}")
+
+    q = deque([(0, *pos)])
+    cheat_visisted = set()
+
+    while q:
+        steps, r, c = q.popleft()
+
+        for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+            if (nr, nc) in visited and visited[(nr,nc)] > visited[pos] and (pos,(nr,nc)) not in cheats:
+                cheats[pos,(nr,nc)] = visited[(nr,nc)] - visited[pos] - steps - 1
+                continue
+
+            if (nr, nc) not in inner_grid or (nr,nc) in cheat_visisted or steps >= 20:
+                continue
+
+            q.append((steps + 1, nr, nc))
+            cheat_visisted.add((nr, nc))
+
+print({(start, end): s for (start, end), s in cheats.items() if start == (3,1)})
+# print({k: v for k, v in cheats.items() if v >= 70})
+print(sum(v for k, v in Counter(cheats.values()).items() if k >= 70))
+# print(Counter(cheats.values()))
