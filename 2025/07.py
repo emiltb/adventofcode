a9bbe37c8134de9e0ff00d3e762198652fc2d34a
Emil Tveden Bjerglund <emilbp@gmail.com>
2025-12-07 07:58:56 +0100

    2025 day 7

---
+++ 2025/07.py
@@ -0,0 +1,45 @@
+data = [l.strip() for l in open('data/7.in')]
+
+S = {(i, j) for i, r in enumerate(data) for j, c in enumerate(r) if data[i][j] == 'S'}
+splitters = {(i,  j) for i, r in enumerate(data) for j, c in enumerate(r) if data[i][j] == '^'}
+
+r_max = len(data)
+
+def p1(pos, splits = set()):
+    if any(p[0] == r_max for p in pos):
+        return len(splits)
+
+    new_pos = set()
+    for r, c in pos:
+        if (r+1, c) in splitters:
+            new_pos.add((r+1, c-1))
+            new_pos.add((r+1, c+1))
+            splits.add((r+1, c))
+        else:
+            new_pos.add((r+1, c))
+    
+    return p1(new_pos, splits)
+
+
+print(p1(S))
+
+def p2(pos, cache):
+    if pos in cache:
+        return cache[pos]
+    
+    if pos[0] == r_max:
+        return 1
+
+    ans = 0
+    r, c = pos
+    if (r+1, c) in splitters:
+        ans += p2((r+1, c-1), cache)
+        ans += p2((r+1, c+1), cache)
+    else:
+        ans += p2((r+1, c), cache)
+
+    cache[pos] = ans
+    
+    return ans
+
+print(p2([s for s in S][0], {}))
