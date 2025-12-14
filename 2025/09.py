923aa2abc221736c475125ab301fcd90bd9cfd52
Emil Tveden Bjerglund <emilbp@gmail.com>
2025-12-14 16:59:16 +0100

    2025 day 9

--- 2025/09.py
+++ 2025/09.py
@@ -1,5 +1,7 @@
 from itertools import combinations
-import matplotlib.pyplot as plt
+from collections import deque
+from tqdm import tqdm
+
 data = [[int(n) for n in l.strip().split(',')] for l in open('data/9.in')]
 
 def area(a, b):
@@ -8,37 +10,48 @@ def area(a, b):
 P1 = max(area(c1, c2) for c1, c2 in combinations(data, 2))
 print(P1)
 
+all_areas = sorted([(area(c1, c2), c1, c2) for c1, c2 in combinations(data, 2)], reverse=True)
+data = data + [data[0]]
 
-# lines = list(zip(data, data[1:]))
-# print(lines)
+perimiter = set()
+for (r1, c1), (r2, c2) in zip(data, data[1:]):
+    for r in range(min(r1, r2), max(r1, r2) + 1):
+        for c in range(min(c1, c2), max(c1, c2) + 1):
+            perimiter.add((r,c))
 
-# print(min(c[0] for c in data),max(c[0] for c in data))
-# print(min(c[1] for c in data),max(c[1] for c in data))
+_, sr, sc = sorted([(r + c, r, c) for r, c in data])[0]
+S = sr - 1, sc - 1
 
-# for r in range(9):
-#     for c in range(14):
-#         if ([c,r] in data):
-#             print('#', end = "")
-#         else:
-#             print('.', end = "")
-#     print()
+q = deque([S])
+outer_perimeter = {S}
+visited = {S}
+dirs = [(1,0),(0,1),(-1,0),(0,-1)]
+extended_dirs = dirs + [(1,1),(1,-1),(-1,1),(-1,-1)]
+while q:
+    r, c = q.popleft()
 
+    for dr, dc in dirs:
+        next_node = nr, nc = r + dr, c + dc
 
-# for (ac, ar), (bc, br) in combinations(data, 2):
-#     print((ac, ar), (bc, br))
-#     if ac == bc or ar == br:
-#         cc = ac
-#         cr = ar
-#         dc = bc
-#         dr = br
+        if next_node in perimiter or next_node in outer_perimeter or next_node in visited:
+            continue
 
-fig = plt.figure()
-ax = plt.axes()
+        for dr, dc in extended_dirs:
+            next_next_node = nr + dr, nc + dc
+            if next_next_node in perimiter:
+                q.append(next_node)
+                outer_perimeter.add(next_node)
+                visited.add(next_node)
 
-# ax.scatter([c[0] for c in data], [c[1] for c in data])
-
-for (ac, ar), (bc, br) in zip(data, data[1:]):
-    ax.plot([c[0] for c in data], [c[1] for c in data], marker = 'o', markersize = 0.1)
-
-ax.set_aspect('equal', adjustable='box')
-plt.show()
+for total, (r1, c1), (r2, c2) in tqdm(all_areas):
+    corners = [(r1, c1), (r2, c1), (r2, c2), (r1, c2)]
+    corners = corners + [corners[0]]
+    valid = True
+    for (r1, c1), (r2, c2) in zip(corners, corners[1:]):
+        border = {(r,c) for r in range(min(r1, r2), max(r1, r2) + 1) for c in range(min(c1, c2), max(c1, c2) + 1)}
+        if border & outer_perimeter:
+            valid = False
+            break
+    if valid:
+        print(total)
+        break
